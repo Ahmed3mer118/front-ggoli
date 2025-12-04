602df02883c75364eda9961a76ce55b80b081eee
Ahmed Amer <amer73090@gmail.com>
2025-12-04 13:20:15 +0200

    add-env-file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    apiUrl: 'https://back-end-ggoli-production.up.railway.app/api',
+    apiImage:'https://back-end-ggoli-production.up.railway.app/uploads'
+  };
+  
